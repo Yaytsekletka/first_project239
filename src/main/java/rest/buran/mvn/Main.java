fa98586919b526164de6c079d00aa7a199c586d2
profi <profiskpro@gmail.com>
2022-12-20 22:46:33 +0300

    java doc

--- src/main/java/rest/buran/mvn/Main.java
+++ src/main/java/rest/buran/mvn/Main.java
@@ -3,10 +3,16 @@ package rest.buran.mvn;
 import app.Application;
 import io.github.humbleui.jwm.App;
 
+/**
+ * Главный класс приложения
+ */
 public class Main {
-
+    /**
+     * Главный метод приложения
+     *
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         App.start(Application::new);
     }
-
 }

1d7f5748ce3ec257efc14e160144325af3b04492
ulyanovaa.24 <profiskpro@gmail.com>
2023-01-12 12:50:02 +0300

    finished 09 prototip

---
+++ src/main/java/panels/PanelRendering.java
@@ -0,0 +1,83 @@
+package panels;
+
+import app.Point;
+import app.Task;
+import io.github.humbleui.jwm.Event;
+import io.github.humbleui.jwm.Window;
+import io.github.humbleui.skija.Canvas;
+import Misc.CoordinateSystem2d;
+import Misc.CoordinateSystem2i;
+import Misc.Vector2d;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+/**
+ * Панель рисования
+ */
+public class PanelRendering extends GridPanel {
+    /**
+     * Представление проблемы
+     */
+    public static Task task;
+
+    /**
+     * Панель управления
+     *
+     * @param window     окно
+     * @param drawBG     флаг, нужно ли рисовать подложку
+     * @param color      цвет подложки
+     * @param padding    отступы
+     * @param gridWidth  кол-во ячеек сетки по ширине
+     * @param gridHeight кол-во ячеек сетки по высоте
+     * @param gridX      координата в сетке x
+     * @param gridY      координата в сетке y
+     * @param colspan    кол-во колонок, занимаемых панелью
+     * @param rowspan    кол-во строк, занимаемых панелью
+     */
+    public PanelRendering(
+            Window window, boolean drawBG, int color, int padding, int gridWidth, int gridHeight,
+            int gridX, int gridY, int colspan, int rowspan
+    ) {
+        super(window, drawBG, color, padding, gridWidth, gridHeight, gridX, gridY, colspan, rowspan);
+
+        // ОСК от [-10.0,-10.0] до [10.0,10.0]
+        CoordinateSystem2d cs = new CoordinateSystem2d(
+                new Vector2d(-10.0, -10.0), new Vector2d(10.0, 10.0)
+        );
+
+        // создаём массив случайных точек
+        ArrayList<Point> points = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            // получаем случайное множество
+            Point.PointSet pointSet = ThreadLocalRandom.current().nextBoolean() ?
+                    Point.PointSet.FIRST_SET : Point.PointSet.SECOND_SET;
+            // добавляем точку в случайном месте ОСК в указанное множество
+            points.add(new Point(cs.getRandomCoords(), pointSet));
+        }
+        task = new Task(cs, points);
+
+    }
+
+    /**
+     * Обработчик событий
+     *
+     * @param e событие
+     */
+    @Override
+    public void accept(Event e) {
+
+    }
+
+    /**
+     * Метод под рисование в конкретной реализации
+     *
+     * @param canvas   область рисования
+     * @param windowCS СК окна
+     */
+    @Override
+    public void paintImpl(Canvas canvas, CoordinateSystem2i windowCS) {
+        task.paint(canvas, windowCS);
+    }
+}
